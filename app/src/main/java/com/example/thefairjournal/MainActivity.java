c9ea7f44763444fea4da1a291974adef194a796b
jonasns <jonas@Jonass-MacBook-Air.local>
2019-11-11 21:03:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/thefairjournal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thefairjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
