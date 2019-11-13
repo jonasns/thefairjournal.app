2bf1f5ddc43ba9242551dee2bd61dc6c896e0e09
jonasns <jonas.sondergaard@ki.se>
2019-11-13 08:06:16 +0100

    Fixed issues

--- app/src/main/java/com/example/thefairjournal/MainActivity.java
+++ app/src/main/java/com/example/thefairjournal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thefairjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.thefairjournal.R;
 
 public class MainActivity extends AppCompatActivity {
 
